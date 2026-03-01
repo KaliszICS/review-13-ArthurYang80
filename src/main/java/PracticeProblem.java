c66598b44457396b05f4a72de31d11d44b4dddb1
ArthurYang80 <342802865@tdsb.ca>
2026-03-01 01:11:05 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,4 +6,38 @@ public class PracticeProblem {
 
 	}
 
+	public static void swap(ArrayList<String> list, int index1, int index2) {
+    String temp = list.get(index1);
+    list.set(index1, list.get(index2));
+    list.set(index2, temp);
+	}
+
+
+	public static ArrayList<Double> createArrayList(double[] arr) {
+    ArrayList<Double> list = new ArrayList<>();
+
+    	for (int i = 0; i < arr.length; i++) {
+        	list.add(arr[i]);
+    	}
+
+    return list;
+	}		
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+    HashMap<String, Integer> map = new HashMap<>();
+
+    	for (int i = 0; i < names.length; i++) {
+        	map.put(names[i], ages[i]);
+    	}
+
+    return map;
+	}
+
+	public static void increaseAge(HashMap<String, Integer> map, String name) {
+    	if (map.containsKey(name)) {
+        	int currentAge = map.get(name);
+        	map.put(name, currentAge + 1);
+    	}
+	}
+
 }
